a7cffa457cda6fc02fbccf6852e35a6d43a1e5ef
test-dan-run <daniel.leongsj@gmail.com>
2024-03-12 01:34:59 +0800

    feat: add asr module

---
+++ modules/speech_recognition/src/serve.py
@@ -0,0 +1,71 @@
+""" ASR server scripts """
+
+import base64
+import logging
+from concurrent import futures
+
+import grpc
+import torch
+import numpy as np
+from nemo.collections.asr.models import EncDecCTCModelBPE
+
+import asr_pb2
+import asr_pb2_grpc
+
+if torch.cuda.is_available():
+    DEVICE = [0]  # use 0th CUDA device
+    ACCELERATOR = 'gpu'
+else:
+    DEVICE = 1
+    ACCELERATOR = 'cpu'
+
+MAP_LOCATION = torch.device(f'cuda:{DEVICE[0]}' if ACCELERATOR == 'gpu' else 'cpu')
+
+class ASRServer(asr_pb2_grpc.SpeechRecognizerServicer):
+    """ASR Model Server instance"""
+
+    def __init__(self):
+        self.model = EncDecCTCModelBPE.restore_from(restore_path="/asr/models/parakeet-ctc-0.6b.nemo")
+
+    def recognize(self, request_iterator: asr_pb2.RecognizeRequest, context):
+        """Takes in audio file and generates transcription, returns string"""
+
+        b64encoded = ""
+        for chunk in request_iterator:
+            b64encoded += chunk.buffer
+
+        b64decoded = base64.b64decode(b64encoded)
+        audio_array = np.frombuffer(b64decoded, dtype=np.float32)
+
+        audio_tensor = torch.from_numpy(audio_array)
+        audio_length_tensor = torch.tensor([audio_tensor.size(dim=0)])
+
+        audio_tensor = audio_tensor.unsqueeze(0)
+
+        logits, logits_len, _ = self.model.forward(
+                            input_signal=audio_tensor.to(MAP_LOCATION), 
+                            input_signal_length=audio_length_tensor.to(MAP_LOCATION),
+                        )
+        
+        hypotheses, _ = self.model.decoding.ctc_decoder_predictions_tensor(
+                            logits, decoder_lengths=logits_len,
+                        )
+
+        return asr_pb2.RecognizeResponse(transcription=hypotheses[0])
+
+
+def serve():
+    """ Serves VAD Model """
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    asr_pb2_grpc.add_SpeechRecognizerServicer_to_server(ASRServer(), server)
+    server.add_insecure_port("[::]:50053")
+    server.start()
+
+    print("Server started, listening on port 50053")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    serve()
---
+++ tests/test_client.py
@@ -0,0 +1,78 @@
+""" Script to test VAD model serving instance 
+
+# generate protobufs
+python3 -m grpc_tools.protoc -I ./proto \
+    --python_out=./tests \
+    --pyi_out=./tests \
+    --grpc_python_out=./tests \
+    ./proto/base.proto \
+    ./proto/asr.proto \
+    ./proto/vad.proto
+
+"""
+
+import os
+import base64
+import logging
+import librosa
+
+import grpc
+import asr_pb2, asr_pb2_grpc
+import vad_pb2, vad_pb2_grpc
+
+CHUNK_SIZE = 1024 * 1024
+SAMPLE_AUDIO_PATH = "../examples/test_audio.wav"
+SAMPLE_RATE = 16000
+
+def get_file_chunks(filepath: str):
+    """ Splits audio file into chunks """
+
+    with open(filepath, "rb") as f:
+        while True:
+            piece = f.read(CHUNK_SIZE)
+            if len(piece) == 0:
+                return
+            yield vad_pb2.DetectRequest(buffer=piece)
+
+def get_encoded_chunks(encoded: str):
+
+    idx = 0
+
+    while True:
+        piece = encoded[idx*CHUNK_SIZE:(idx+1)*CHUNK_SIZE]
+        if len(piece) == 0:
+            return
+        idx += 1
+        yield asr_pb2.RecognizeRequest(buffer=piece)
+
+def run():
+    """ Sends audio file to model serving instance """
+
+    with grpc.insecure_channel("localhost:50052") as channel:
+        stub = vad_pb2_grpc.VoiceActivityDetectorStub(channel)
+        chunks_generator = get_file_chunks(SAMPLE_AUDIO_PATH)
+        response = stub.detect(
+            chunks_generator,
+            metadata=(("filename", os.path.basename(SAMPLE_AUDIO_PATH)),),
+        )
+
+    # decode bytes to array, reshape is needed as frombuffer outputs 1d-array
+    timestamps = response.timestamps
+    audio_arr, _ = librosa.load(SAMPLE_AUDIO_PATH, sr=SAMPLE_RATE, mono=True)
+    for ts in timestamps:
+
+        with grpc.insecure_channel("localhost:50053") as channel:
+            stub = asr_pb2_grpc.SpeechRecognizerStub(channel)
+
+            start, end = int(SAMPLE_RATE * ts.start), int(SAMPLE_RATE * ts.end) 
+            segment = audio_arr[start:end]
+
+            b64encoded = base64.b64encode(segment)
+            chunks_generator = get_encoded_chunks(b64encoded)
+            response = stub.recognize(chunks_generator)
+            print(response.transcription)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
